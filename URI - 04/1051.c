e682c78d0837c791096447b1a266adbb3787e6ff
Yuri <xavier.ysx@gmail.com>
2020-06-13 17:26:07 -0300

    1051 added but no finished

---
+++ URI - 04/1051.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float salary, tax;
+
+    scanf("%f", &salary);
+
+    if (0.00 < salary <= 2000.00){
+        printf("Isento\n");
+    }
+    // IN PROGRESSSSS
+    if(salary>4500.00){
+    tax = ((28/100) * salary) + salary;
+    printf("R$: %.2f\n", tax);
+    }
+    return 0;
+}
