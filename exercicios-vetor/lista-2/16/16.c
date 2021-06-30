f10fd9daa6f479849526df1cccbf121956ae10a8
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-06-30 18:15:57 -0300

    C file 16

---
+++ exercicios-vetor/lista-2/16/16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int U[10], aux=0, count=9;
+
+	printf("Insira o valor do vetor: \n");
+	for (int index = 0; index < 10; ++index){
+		scanf("%d", &U[index]);
+	}
+	for (int index = 0; index < 10; ++index){
+		if (index < 5) {
+			aux = U[index];
+			U[index] = U[index + 5];
+			U[index + 5] = aux;
+		}
+	}
+
+	for (int index = 0; index < 10; ++index){
+		printf("%d ", U[index]);
+	}
+
+
+	return 0;
+}
