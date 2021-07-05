a5438a1536aee3562441051acb8c2558a4aa135a
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-07-05 20:46:50 -0300

    add 16 exercise

---
+++ exercicios-vetor/16/16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int R[5], M[10] aux=0;
+
+	printf("Insira o valor do vetor: \n");
+	for (int index = 0; index < 10; ++index){
+		scanf("%d", &M[index]);
+	}
+	for (int index = 0; index < 10; ++index){
+		if (index < 5) {
+			aux = M[index];
+			M[index] = M[index + 5];
+			M[index + 5] = aux;
+		}
+	}
+
+	for (int index = 0; index < 10; ++index){
+		printf("%d ", M[index]);
+	}
+
+
+	return 0;
+}
---
+++ exercicios-vetor/16/README.md
@@ -0,0 +1,4 @@
+Ler um vetor M de 10 elementos. Troque a seguir o 1o. elemento com o 6o., o 2o. com
+o 7o.,etc. até o 5o. com 10o. e escreva o vetor M assim modificado.
+
+![](https://github.com/Yxav/proglogic/blob/master/exercicios-vetor/16/16.png)
