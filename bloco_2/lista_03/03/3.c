cb4f186a59c552623e6297506c8800f71f0c8ba9
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-07-20 00:31:50 -0300

    exercise 3 -> lista03

---
+++ bloco_2/lista_03/03/3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define MAX_MATRIX 3
+
+
+int main(int argc, char const *argv[])
+{
+	int M[MAX_MATRIX][MAX_MATRIX];
+
+
+
+	printf("Insira o valor da matriz abaixo\n");
+	for (int line = 0; line < MAX_MATRIX; ++line)
+	{
+		for (int column = 0; column < MAX_MATRIX; ++column)
+		{
+			scanf("%d", &M[line][column]);
+		}
+	}
+	printf("\n");
+
+	for (int line = 0; line < MAX_MATRIX; ++line)
+	{
+		for (int column = 0; column < MAX_MATRIX; ++column)
+		{
+				printf("%d ",M[line][column]*2);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ bloco_2/lista_03/03/README.md
@@ -0,0 +1,2 @@
+3) Crie um programa em C que leia os elementos de uma matriz inteira de 3 x 3 e
+imprima outra matriz multiplicando cada elemento da primeira matriz por 2
